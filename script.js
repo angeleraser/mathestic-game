4c1f58a9e432abfc9942ac46fd47a519bbd59715
angeleraser <angelfiguera117@gmail.com>
2020-02-26 23:29:38 -0400

    Mathestic1.02

--- script.js
+++ script.js
@@ -1,152 +1,202 @@
-const
-  button_key= document.querySelectorAll('.button-key'),
-  button= document.querySelectorAll('.button'),
-  input= document.getElementById('input'),
-  enter= document.getElementById('enter'),
-  erase_button= document.getElementById('erase'),
-  container= document.getElementById('container'),
-  main_screen_buttons= document.querySelectorAll('.main-screen'),
-  game_screen= document.querySelectorAll('.game-screen'),
-  answers= [2,50,28,12,20,13,324,10,26,13260],
-  levels= document.getElementsByClassName('level'),
-  social= document.getElementById('social'),
-  back= document.getElementById('backnote'),
-  hints= document.querySelectorAll('.hint_img'),
-  hint_button= document.getElementById('hint'),
-  idea_icon=document.getElementById('icon'),
-  keys= document.querySelectorAll('.key'),
-  sounds=document.getElementsByClassName('sounds'),
-  keyboard=document.getElementById('keyboard'),
-  about=document.getElementById('about'),
-  win_sound=document.getElementById('win-sound'),
-  level_counter=document.getElementById('level-counter');
-// para los clicks
-button_key.forEach(key=>{
-  key.addEventListener('click',()=>{
-    input.value=`${input.value}${key.innerHTML}`;
+// Niveles y pista 
+const levels = [...document.querySelectorAll('.level')],
+      hints = [...document.querySelectorAll('.hint_img')],
+      // array con todas las respuestas 
+      answers = [2,50,28,12,20,13,324,10,26,13260,820,65536,'10',60,196];
+ //Botones
+const answer_input = document.getElementById('input'),
+      enter = document.getElementById('enter'),
+      hint_button = document.getElementById('hint-button'),
+      hint_img = document.getElementById('icon'),
+      key = [...document.querySelectorAll('.key')],
+      erase_button = document.getElementById('erase-button'),
+      screen_button = [...document.querySelectorAll('.screen-button')],
+      back_button = document.getElementById('back-button');
+// Sonidos 
+const sounds = [...document.querySelectorAll('.sound')],
+      menu_back_sound = sounds[0],
+      section_fail_sound = sounds[1],
+      section_pass = sounds[2],
+      hint_sound =  sounds[3],
+      menu_button_click = sounds[4],
+      win_sound = sounds[5];
+// Pantallas
+const screens = [...document.querySelectorAll('.screen-game')],
+      final_screen = document.getElementById('final-screen');
+const level_counter=document.getElementById('level-counter');
+// CONTENEDOR 
+const container = document.getElementById('container');
+      // efecto de error 
+      container.shaking = ()=>{
+        if(levelIndex >= LEVEL.length){
+          container.classList.add('error');
+          playSound(win_sound);
+          setTimeout(()=>{
+            container.classList.remove('error')
+          },6500)
+        }
+        else{
+        container.classList.add('error');
+        playSound(section_fail_sound);
+        setTimeout(()=>{
+          container.classList.remove('error')
+        },300)
+        }
+      }
+// Clase para construir los niveles 
+class Level {
+  constructor(name,lvl,hint,ans){
+    // nombre 
+    this.name=`Level ${name+1}`;
+    // imagen del nivel 
+    this.level=lvl;
+    // imagen de la pista 
+    this.hint=hint;
+    // respuesta 
+    this.answer=ans;
+  }
+  // mostrar nivel
+  show(){
+    this.level.style.display='block';
+    this.hint.style.display='block'
+  }
+  // ocultar nivel
+  hidden(){
+    this.level.style.display='none';
+    this.hint.style.transform='translateX(0%)';
+  }
+  // mostrar y ocultar pista 
+  showHint(){
+    if (innerWidth <= 1100 ){
+      this.hint.style.zIndex='30'
+      this.hint.style.transform = this.hint.style.transform == 'scale(1)'? 'scale(.001)' : 'scale(1)';
+    }
+    else{
+      this.hint.style.zIndex='-10'
+      this.hint.style.transform = this.hint.style.transform == 'translateX(100%)'? 'translateX(0%)' : 'translateX(100%)';
+    }
+    // ENCENDER/APAGAR BOMBILLO DE PISTA 
+    if(this.hint.style.transform == 'scale(1)' || this.hint.style.transform == 'translateX(100%)'){
+      hint_img.src='./icons/idea2.png'
+    }
+    else{
+      hint_img.src='./icons/idea.png'
+    }
+  }
+  // OCULTAR PISTA CUANDO SE PASA DE NIVEL 
+  hiddenHint(){
+    this.hint.style.display='none';
+  }
+}
+// Clase para las pantallas 
+class Screen {
+  constructor(name,scr){
+    this.name=`Screen ${name + 1}`;
+    this.screen= scr;
+  }
+  showScreen(){
+    this.screen.classList.remove('hidden');
+  }
+  hiddenScreen(){
+    this.screen.classList.add('hidden');
+  }
+}
+const SCREEN=[]; //Array con todas las pantallas
+const LEVEL=[]; // Array con todos los niveles
+// Funcion constructora del nivel que toma como parametros tres listas : nivel, pistas y respuestas
+function createLevel(lvl,hnt,ans){
+  // Nombre e imagen del nivel 
+  lvl.forEach( l => {
+    let lvl= new Level(levels.indexOf(l),l);
+    LEVEL.push(lvl);
   });
-});
-// indice que sincroniza nivel y pistas 
-let index=0;
-// click en enter 
+  // Imagen de la pista 
+  hnt.forEach( h => {
+    LEVEL[hints.indexOf(h)].hint=h;
+  })
+  // respuesta 
+  ans.forEach( a  => {
+    LEVEL[answers.indexOf(a)].answer=a;
+  })
+};
+createLevel(levels,hints,answers);
+function createScreen(scn){
+  scn.forEach( s =>{
+    let screen= new Screen(screens.indexOf(s), s);
+    SCREEN.push(screen);
+  })
+};
+createScreen(screens);
+// Sonidos 
+function playSound(s){
+  s.currentTime=0;
+  s.play();
+}
+const main_screen = SCREEN[0],game_screen = SCREEN [1];
+// AQUI EMPIEZAN LAS INTERACCIONES 
+let levelIndex=0;
+// Click enter
 enter.addEventListener('click',()=>{
-  if(input.value==answers[index]){
-    index++;
-    levels[index].style.display='block';
-    hints[index-1].style.transform='translateX(0%)';
-    input.value="";
-    sound(sounds[2]);
+  if(answer_input.value == LEVEL[levelIndex].answer){
+    levelIndex++;
+    if(levelIndex >= LEVEL.length){
+        LEVEL[levelIndex-1].hidden();
+        game_screen.hiddenScreen();
+        final_screen.classList.remove('hidden');
+        container.shaking();
+        answer_input.value="";
+        levelIndex=0;
+    }
+    else{
+    LEVEL[levelIndex].show()
+    levelIndex > 0 ? LEVEL[levelIndex - 1].hidden() : LEVEL[levelIndex].show();
+    answer_input.value="";
+    playSound(section_pass);
+    LEVEL[levelIndex - 1].hiddenHint();
+    level_counter.innerHTML=`${LEVEL[levelIndex].name}`;
+    }
   }
+  // RESPUESTA EQUIVOCADA 
   else{
-    input.value="";
-    sound(sounds[1]);
-    container.classList.add('error');
+    answer_input.value="";
+    answer_input.setAttribute('placeholder','WRONG!');
     setTimeout(()=>{
-      container.classList.remove('error');
-    },300)
+      answer_input.setAttribute('placeholder','ANSWER')
+    },1000);
+    container.shaking();
   }
- for (let index2 = 0; index2 < levels.length; index2++) {
-  if(index2==answers.indexOf(answers[index])){
-    continue;
-  }
-  levels[index2].style.display='none';
- };
- if(index==10){
-   levels[index].style.display='block';
-   keyboard.style.display='none';
-   sound(sounds[5]);
-   level_counter.style.display='none';
-   container.classList.add('error')
-   setTimeout(()=>{
-    container.classList.remove('error');
-  },7000)
-  }
- if(innerWidth<=1024){
-  back.style.display='inline-block'
-  hints[index-1].style.transform='scale(.0001)'
-}
-else{
-  hints[index].style.transform == 'translateX(100%)' ?  idea_icon.src='./icons/idea2.png' : idea_icon.src='./icons/idea.png';
-}
-level_counter.innerHTML=`Level ${index + 1}`
 });
-// hint button 
-hint_button.addEventListener('click',()=>{
-  if(innerWidth<=1024){  
-    hints[index].style.transform = hints[index].style.transform == 
-                                                                  'scale(1)'?
-                                                                  'scale(.001)':
-                                                                  'scale(1)';
-    hints[index].style.zIndex='10';
-    hints[index].style.transform == 'scale(1)' ?  idea_icon.src='./icons/idea2.png' : idea_icon.src='./icons/idea.png';
-  }
-  else{
-    hints[index].style.transform = hints[index].style.transform == 
-                                                                'translateX(100%)'?
-                                                                'translateX(0%)':
-                                                                'translateX(100%)';
-   hints[index].style.transform == 'translateX(100%)' ?  idea_icon.src='./icons/idea2.png' : idea_icon.src='./icons/idea.png';                                                            
-  }
-  sound(sounds[3]);
+// boton de pista 
+hint_button.addEventListener('click', ()=>{
+  LEVEL[levelIndex].showHint();
+  playSound(hint_sound);
 });
-// borrar
-erase_button.addEventListener('click', ()=>{
-  input.value="";
+// boton de X para borrar 
+erase_button.addEventListener('click',()=>{
+  answer_input.value=``;
 })
-// botones de la pantalla principal 
-for (let index = 0; index < main_screen_buttons.length; index++) {
-  // boton play
-  if(index==1){
-    main_screen_buttons[index].addEventListener('click',()=>{
-      sound(sounds[4]);
-      main_screen_buttons.forEach((btn=>{
-        btn.classList.add('hidden');
-        input.value="";
-      }));
-      game_screen.forEach((elmnt=>{
-        elmnt.classList.remove('hidden');
-      }));
-      back.style.display='inline-block';
-    })
-  }
-  // boton follow 
-  if(index==2){
-    main_screen_buttons[index].addEventListener('click',()=>{
-      sound(sounds[4]);
-      social.classList.remove('hidden');
-      main_screen_buttons.forEach((btn=>{
-        btn.classList.add('hidden');
-      }));
-      back.style.display='block'
-    });                                                         
-  };
-  if(index==3){
-    main_screen_buttons[index].addEventListener('click',()=>{
-      about.classList.remove('hidden')
-      sound(sounds[4]);
-      main_screen_buttons.forEach((btn=>{
-        btn.classList.add('hidden');
-      }));
-      back.style.display='block';
-    });
-  }
-};
-// boton de regresar 
-back.addEventListener('click',()=>{
-  sound(sounds[0]);
-  back.style.display='none';
-  game_screen.forEach((elmnt=>{
-    elmnt.classList.add('hidden');
-  main_screen_buttons.forEach((btn=>{
-    btn.classList.remove('hidden');
-    }))
-  }));
-  social.classList.add('hidden');
-  about.classList.add('hidden');
-  hints[index].style.transform == 'translateX(100%)' ?  idea_icon.src='./icons/idea2.png' : idea_icon.src='./icons/idea.png';                                                            
+// Numeros 
+key.forEach( k => {
+  k.addEventListener('click',()=>{
+    answer_input.value = `${answer_input.value}${k.innerHTML}`
+  })
 });
-// sonido 
-function sound(sd){
-  sd.currentTime=0;
-  sd.play();
-};
+// Cambio entre pantallas con los botones principales 
+screen_button.forEach( btn =>{
+  btn.addEventListener('click', ()=>{
+    main_screen.hiddenScreen();
+    SCREEN[screen_button.indexOf(btn)+1].showScreen();
+    back_button.style.display = back_button.style.display == 'inline-block' ? 'none' : 'inline-block';
+    playSound(menu_button_click);
+    level_counter.innerHTML=`${LEVEL[levelIndex].name}`;
+    LEVEL[levelIndex].show();
+    final_screen.classList.add('hidden');
+  });
+  back_button.addEventListener('click',()=>{
+    back_button.style.display = back_button.style.display == 'inline-block' ? 'none' : 'inline-block';
+    main_screen.showScreen();
+    SCREEN[screen_button.indexOf(btn)+1].hiddenScreen();
+    playSound(menu_back_sound);
+    final_screen.classList.add('hidden');
+  })
+});
